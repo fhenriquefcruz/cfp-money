7035bcc327692c36678a213ae0fe9bebd629a59b
Fabio Henrique <59840567+fhenriquefcruz@users.noreply.github.com>
2026-06-05 09:22:46 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cfp-money/',          // <--- caminho do repositório
+  base: '/cfp-money/',
   build: {
     outDir: 'dist',
     sourcemap: false,
