85e364ba7b63fe3ab645829adbe87112f3f46f0d
Fabio Henrique <59840567+fhenriquefcruz@users.noreply.github.com>
2026-07-27 18:46:07 +0000

    Updates

---
+++ assets/settings-2-YVoUvSVY.js
@@ -0,0 +1,11 @@
+import{c as e}from"./index-DKLvNzng.js";/**
+ * @license lucide-react v0.330.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=e("RotateCcw",[["path",{d:"M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8",key:"1357e3"}],["path",{d:"M3 3v5h5",key:"1xhq8a"}]]);/**
+ * @license lucide-react v0.330.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=e("Settings2",[["path",{d:"M20 7h-9",key:"3s1dr2"}],["path",{d:"M14 17H5",key:"gfn3mx"}],["circle",{cx:"17",cy:"17",r:"3",key:"18b49y"}],["circle",{cx:"7",cy:"7",r:"3",key:"dfmy0x"}]]);export{c as R,a as S};
--- assets/startOfMonth-CRyvdT-k.js
+++
@@ -1,6 +0,0 @@
-import{c as s}from"./index-BX6VgVoZ.js";import{t as n}from"./index-CVlZ9_pn.js";/**
- * @license lucide-react v0.330.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const i=s("TrendingDown",[["polyline",{points:"22 17 13.5 8.5 8.5 13.5 2 7",key:"1r2t7k"}],["polyline",{points:"16 17 22 17 22 11",key:"11uiuu"}]]);function u(e){const t=n(e),o=t.getMonth();return t.setFullYear(t.getFullYear(),o+1,0),t.setHours(23,59,59,999),t}function c(e){const t=n(e);return t.setDate(1),t.setHours(0,0,0,0),t}export{i as T,u as e,c as s};
